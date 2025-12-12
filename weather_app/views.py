1a9dbe611cefb835a027508deca073522fbbafa6
Denis Pleshakov <satone@live.ru>
2025-12-12 10:12:26 +0300

    Update views.py

--- weather_app/views.py
+++ weather_app/views.py
@@ -4,10 +4,9 @@ from django.http import JsonResponse, HttpResponse
 
 def index(_):
     CITY = os.getenv('CITY', 'Moscow')
-    base_url = 'https://geocoding-api.open-meteo.com/v1/'
-    json_geo = requests.get(f'{base_url}search?name={CITY}').json()
+    json_geo = requests.get(f'https://geocoding-api.open-meteo.com/v1/search?name={CITY}').json()
     lat, lon = json_geo['results'][0]['latitude'], json_geo['results'][0]['longitude']
-    url = f"{base_url}forecast?latitude={lat}&longitude={lon}&current_weather=true&temperature_unit=celsius"
+    url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true&temperature_unit=celsius"
     try:
         (response := requests.get(url)).raise_for_status()
         return HttpResponse(f"<h2>{response.json()['current_weather']['temperature']}</h2>")
